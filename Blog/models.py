a638b1072ddd00ad3dee36e41d0a3a2821fa847c
scarmody402 <35843068+scarmody402@users.noreply.github.com>
2022-04-29 17:20:19 -0500

    Another Commit

--- Blog/models.py
+++ Blog/models.py
@@ -16,7 +16,21 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['created_on']
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('created_on')
@@ -7,4 +9,5 @@ class PostList(generic.ListView):
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'post_detail.html'    
+    
